db321df436173addc868245130389c45d82c1852
Arnova-1 <arnova124@gmail.com>
2026-01-13 17:40:07 +0700

    register the stick

---
+++ src/main/kotlin/org/arnova/worldinspectorstick/WorldInspectorMod.kt
@@ -0,0 +1,42 @@
+package org.arnova.worldinspectorstick
+
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroups
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.util.Identifier
+import org.arnova.worldinspectorstick.item.WorldInspectorStick
+
+object WorldInspectorMod : ModInitializer {
+
+    const val MOD_ID = "worldinspectorstick"
+
+    val WORLD_INSPECTOR_STICK_ID: Identifier? =
+        Identifier.of(MOD_ID, "world_inspector_stick")
+
+    override fun onInitialize() {
+        val INSPECTOR_STICK = Registry.register(
+            Registries.ITEM,
+            WORLD_INSPECTOR_STICK_ID,
+            WorldInspectorStick(
+                Item.Settings()
+                    .maxCount(1)
+                    .registryKey(
+                        RegistryKey.of(
+                            Registries.ITEM.key,
+                            WORLD_INSPECTOR_STICK_ID
+                        )
+                    )
+            )
+        )
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL)
+            .register { entries ->
+                entries.add(INSPECTOR_STICK)
+            }
+    }
+}
+
--- src/main/kotlin/org/arnova/worldinspectorstick/WorldInspectorStick.kt
+++
@@ -1,9 +0,0 @@
-package org.arnova.worldinspectorstick
-
-import net.fabricmc.api.ModInitializer
-
-class WorldInspectorStick : ModInitializer {
-
-    override fun onInitialize() {
-    }
-}
